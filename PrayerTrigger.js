93aaad77b7cbe85552d4c886b702d0b2317863be
lahoreia <abdulrafay@gmail.com>
2018-01-04 12:30:23 -0600

    Create PrayerTrigger.js

---
+++ PrayerTrigger.js
@@ -0,0 +1,37 @@
+
+var http = require('http');
+var url = require('url');
+
+let key = 'c1wNEbBZ6AyOGKQTp4DPJR';
+
+
+function triggerIftttMakerWebhook(event, key, value1, value2, value3) {
+  let iftttNotificationUrl = `https://maker.ifttt.com/trigger/${event}/with/key/${key}`;
+  let postData = JSON.stringify({ value1, value2, value3 });
+
+  var parsedUrl = url.parse(iftttNotificationUrl);
+  var post_options = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.path,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('Response: ' + chunk);
+      });
+  });
+
+  // Trigger a POST to the url with the body.
+  post_req.write(postData);
+  post_req.end();
+}
+
+triggerIftttMakerWebhook('prayer_call', key);
